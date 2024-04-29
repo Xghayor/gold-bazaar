baead69cd14bcf2f36cbbdeb8699f4d9c982cc7a
Xghayor <cc.baqir@gmail.com>
2024-04-29 15:04:02 +0300

    fix controllers bugs

--- app/controllers/order_details_controller.rb
+++ app/controllers/order_details_controller.rb
@@ -5,14 +5,18 @@ class OrderDetailsController < ApplicationController
       @product = Product.find_by(id: order_detail_param[:product_id])
       @order_detail = @order.order_details.build(order_detail_param)
       @order_detail.product = @product
-      @order_detail.create
+      if @order_detail.save
+        render json: {status: "created"}, status: 201
+      else
+        render json: {status: "unprocessable_entity"}, status: 422
+      end
     end
   end
 
   private
 
   def order_detail_params
-    params.require(:order_detail).map do |param|
+    params.require(:order_details).map do |param|
       param.permit(:quantity, :unitprice, :product_id)
     end
   end
